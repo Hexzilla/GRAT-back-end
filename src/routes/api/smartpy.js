6561b527122932ce531163509ad9f16d27173e8f
Eric Thomas <bluebird6922@gmail.com>
2022-05-06 07:26:12 +0300

    update route

--- src/routes/api/smartpy.js
+++ src/routes/api/smartpy.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 router.get('/', function(req, res, next){
-  console.log('smartpy')
+  console.log('smartpy');
+  res.json({ success: true })
 });
 
 module.exports = router;
