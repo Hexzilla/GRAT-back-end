077ebf4716070fce6afb4c44769934b48ba7f023
Eric Thomas <bluebird6922@gmail.com>
2022-05-06 08:54:15 +0300

    compile

--- src/routes/blockly.js
+++ src/routes/blockly.js
@@ -7,9 +7,50 @@ router.get('/', (req, res) => {
   res.json({ success: true })
 });
 
-router.post('/', 
+const getUserDir = (taqId) => {
+  const rootDir = path.dirname(require.main.filename);
+  return `${rootDir}/../storage/${taqId}`;
+}
+
+router.post(
+  '/', 
   body('name').isString(),
   body('code').isString(),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const taqId = '4d443421-46da-492f-b8f6-31fc8d7b09bb';//req.cookies.taqId;
+      console.log('taqId', taqId);
+      if (!taqId) {
+        return res.status(400).json({ message: 'Invalid taqId'});
+      }
+
+      const {name, code} = req.body;
+      console.log('name-code', name, code);
+
+      const userDir = getUserDir(taqId);
+      const fileDir = `${userDir}/contracts`;
+      await fs.mkdir(fileDir, { recursive: true });
+
+      const filePath = `${fileDir}/${name}.py`;
+      console.log('filePath', filePath);
+
+      await fs.writeFile(filePath, code);
+
+      res.json({ success: true })
+    } catch (ex) {
+      console.error(ex);
+      return res.status(400).json({ message: 'system error' });
+    }
+})
+
+router.post(
+  '/compile', 
+  body('name').isString(),
   async (req, res) => {
     try {
       const errors = validationResult(req);
@@ -23,23 +64,31 @@ router.post('/',
         return res.json({ success: false, message: 'Invalid taqId'});
       }
 
-      const {name, code} = req.body;
-      console.log('name-code', name, code);
-
-      const rootDir = path.dirname(require.main.filename);
-      const fileDir = `${rootDir}/../storage/${taqId}/contracts`;
-      await fs.mkdir(fileDir, { recursive: true });
-
-      const filePath = `${fileDir}/${name}.py`;
+      const {name} = req.body;
+      const userDir = getUserDir(taqId);
+      const filePath = `${userDir}/contracts/${name}.py`;
       console.log('filePath', filePath);
 
-      await fs.writeFile(filePath, code);
+      if (!await fs.exists(filePath)) {
+        return res.status(400).json({ message: 'File does not exists'});
+      }
 
-      res.json({ success: true })
+      const configPath = `${userDir}/.taq/config.json`;
+      if (!await fs.exists(configPath)) {
+        await initTaq();
+      }
+
+
+
     } catch (ex) {
       console.error(ex);
-      return res.status(400).json({ success: false, message: 'system error' });
+      return res.status(400).json({ message: 'system error' });
     }
-})
+});
+
+const initTaq = async (dir) => {
+
+}
+
 
 module.exports = router;
