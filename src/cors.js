6c4c4ed3c2faa84eed7bd19530a379b7a1a62a9f
Eric Thomas <bluebird6922@gmail.com>
2022-05-19 02:31:08 +0300

    update cors

--- src/cors.js
+++ src/cors.js
@@ -3,10 +3,16 @@ const cors = require('cors');
 const getApplicationAllowedOrigins = () => {
 	const env = process.env.NODE_ENV;
 	switch (env) {
+		case 'production':
+			return [
+				'https://grat.dev',
+			];
+
 		case 'development':
 			return [
 				'http://localhost:3000',
 				'http://localhost:3300',
+				'https://grat.dev',
 			];
 	}
 };
