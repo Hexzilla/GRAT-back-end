734fb60cbc44990c14caf2ecc1e1f48b577c0191
Eric Thomas <bluebird6922@gmail.com>
2022-05-21 12:27:59 +0300

    update headers

--- src/app.js
+++ src/app.js
@@ -50,6 +50,16 @@ if (!isProduction) {
   app.use(errorhandler());
 }
 
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
+
 /*if(isProduction){
   mongoose.connect(process.env.MONGODB_URI);
 } else {
